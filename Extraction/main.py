0eb6678b739952da9083ee79ca57fa0cac8203d7
Leisy Sánchez <leisysado@gmail.com>
2024-07-04 13:39:04 -0500

    Add base extraction API

--- Extraction/main.py
+++ Extraction/main.py
@@ -1,8 +1,30 @@
+import json
+import requests
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Extracted(BaseModel):
+    name: str
+    abilities: list[str]
+    picture: str
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/extraction/")
+async def extraction(name: str):
+    url = f"https://pokeapi.co/api/v2/pokemon/{name}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        pokemon_context = response.json()
+        abilities_context = pokemon_context["abilities"]
+        abilities = []
+        for ab in abilities_context:
+            abilities.append(ab["ability"]["name"])
+        result = Extracted(name=name,
+                           picture=pokemon_context["sprites"]["other"]["home"]["front_default"],
+                           abilities=abilities)
+    else:
+        result = f"Error on request: {response.status_code}"
+    return result
